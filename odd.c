965db76c898f99a4aa7aaec594b7f7dcb807b289
vengateshp <vengatcse1997@gmail.com>
2016-12-23 14:03:05 +0530

    Add files via upload

---
+++ odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int year;
+printf("Enter the year");
+scanf("%d",&year);
+if(year%4==0 && year%100==0 && year%400==0)
+printf("Given year is leaf");
+else
+printf("Given year is not leaf");
+getch();
+}
